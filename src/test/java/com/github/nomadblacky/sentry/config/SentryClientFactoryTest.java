92cf28ab8bd1572f2c125af8b11d503c0ddf4ee5
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-10-24 19:50:51 +0900

    Improve tests

--- src/test/java/com/github/nomadblacky/sentry/config/SentryClientFactoryTest.java
+++ src/test/java/com/github/nomadblacky/sentry/config/SentryClientFactoryTest.java
@@ -4,157 +4,181 @@ import static org.assertj.core.api.Assertions.*;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import io.sentry.DefaultSentryClientFactory;
-import io.sentry.SentryClient;
 import io.sentry.buffer.Buffer;
 import io.sentry.buffer.DiskBuffer;
-import io.sentry.dsn.Dsn;
-import io.sentry.jvmti.FrameCache;
 import java.io.File;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.Set;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class SentryClientFactoryTest {
 
   private static SentryClientFactory FACTORY;
-  private static SentryClient CLIENT;
 
   @BeforeAll
   static void beforeAll() {
     FACTORY = new SentryClientFactory();
-    CLIENT = FACTORY.createSentryClient(null);
   }
 
   @Test
-  void testToInitializeDsn() {
+  void initializeDsn() {
     assertThat(FACTORY.dsnString).isEqualTo("noop://localhost/1");
   }
 
   @Test
-  void testToInitializeRelease() {
-    assertThat(CLIENT.getRelease()).isEqualTo("0.1.0");
+  void getInAppFrames() {
+    assertThat(FACTORY.getInAppFrames(null)).containsOnly("com.github.nomadblacky", "sample.foo");
   }
 
   @Test
-  void testToInitializeDistribution() {
-    assertThat(CLIENT.getDist()).isEqualTo("x86");
+  void getAsyncEnabled() {
+    assertThat(FACTORY.getAsyncEnabled(null)).isFalse();
   }
 
   @Test
-  void testToInitializeEnvironment() {
-    assertThat(CLIENT.getEnvironment()).isEqualTo("development");
+  void getBufferedConnectionShutdownTimeout() {
+    assertThat(FACTORY.getBufferedConnectionShutdownTimeout(null)).isEqualTo(300000L);
   }
 
   @Test
-  void testToInitializeServerName() {
-    assertThat(CLIENT.getServerName()).isEqualTo("dev-server");
+  void getBufferedConnectionGracefulShutdownEnabled() {
+    assertThat(FACTORY.getBufferedConnectionGracefulShutdownEnabled(null)).isFalse();
   }
 
   @Test
-  void testToInitializeTags() {
+  void getBufferFlushtime() {
+    assertThat(FACTORY.getBufferFlushtime(null)).isEqualTo(200000L);
+  }
+
+  @Test
+  void getAsyncShutdownTimeout() {
+    assertThat(FACTORY.getAsyncShutdownTimeout(null)).isEqualTo(500L);
+  }
+
+  @Test
+  void getAsyncGracefulShutdownEnabled() {
+    assertThat(FACTORY.getAsyncGracefulShutdownEnabled(null)).isFalse();
+  }
+
+  @Test
+  void getAsyncQueueSize() {
+    assertThat(FACTORY.getAsyncQueueSize(null)).isEqualTo(200);
+  }
+
+  @Test
+  void getAsyncPriority() {
+    assertThat(FACTORY.getAsyncPriority(null)).isEqualTo(9);
+  }
+
+  @Test
+  void getAsyncThreads() {
+    assertThat(FACTORY.getAsyncThreads(null)).isEqualTo(3);
+  }
+
+  @Test
+  void getSampleRate() {
+    assertThat(FACTORY.getSampleRate(null)).isEqualTo(0.75);
+  }
+
+  @Test
+  void getProxyPort() {
+    assertThat(FACTORY.getProxyPort(null)).isEqualTo(8888);
+  }
+
+  @Test
+  void getProxyHost() {
+    assertThat(FACTORY.getProxyHost(null)).isEqualTo("proxy.org");
+  }
+
+  @Test
+  void getProxyUser() {
+    assertThat(FACTORY.getProxyUser(null)).isEqualTo("username");
+  }
+
+  @Test
+  void getProxyPass() {
+    assertThat(FACTORY.getProxyPass(null)).isEqualTo("pass");
+  }
+
+  @Test
+  void getRelease() {
+    assertThat(FACTORY.getRelease(null)).isEqualTo("0.1.0");
+  }
+
+  @Test
+  void getDist() {
+    assertThat(FACTORY.getDist(null)).isEqualTo("x86");
+  }
+
+  @Test
+  void getEnvironment() {
+    assertThat(FACTORY.getEnvironment(null)).isEqualTo("development");
+  }
+
+  @Test
+  void getServerName() {
+    assertThat(FACTORY.getServerName(null)).isEqualTo("dev-server");
+  }
+
+  @Test
+  void getTags() {
     assertThat(FACTORY.getTags(null))
         .containsOnly(entry("key1", "value1"), entry("key2", "value2"));
   }
 
   @Test
-  void testToInitializeMdcTags() {
+  void getMdcTags() {
     assertThat(FACTORY.getMdcTags(null)).containsOnly("mdcTag1", "mdcTag2");
   }
 
   @Test
-  void testToInitializeExtraData() {
+  void getExtra() {
     assertThat(FACTORY.getExtra(null))
         .containsOnly(entry("ex1", "exValue1"), entry("ex2", "exValue2"));
   }
 
   @Test
-  void testToInitializeInApplicationStackFrames() throws Exception {
-    Field field = FrameCache.class.getDeclaredField("appPackages");
-    field.setAccessible(true);
-    Set<String> appPackages = (Set<String>) field.get(null);
-    assertThat(appPackages).containsOnly("com.github.nomadblacky", "sample.foo");
+  void getCompressionEnabled() {
+    assertThat(FACTORY.getCompressionEnabled(null)).isFalse();
   }
 
   @Test
-  void testToInitializeHideCommon() {
+  void getHideCommonFramesEnabled() {
     assertThat(FACTORY.getHideCommonFramesEnabled(null)).isFalse();
   }
 
   @Test
-  void testToInitializeEventSamplingRate() throws Exception {
-    Method method = DefaultSentryClientFactory.class.getDeclaredMethod("getSampleRate", Dsn.class);
-    method.setAccessible(true);
-    Double rate = (Double) method.invoke(FACTORY, (Dsn) null);
-    assertThat(rate).isEqualTo(0.75);
+  void getMaxMessageLength() {
+    assertThat(FACTORY.getMaxMessageLength(null)).isEqualTo(2000);
   }
 
   @Test
-  void testToInitializeUncaughtExceptionHandlerEnabled() {
-    assertThat(FACTORY.getUncaughtHandlerEnabled(null)).isFalse();
+  void getTimeout() {
+    assertThat(FACTORY.getTimeout(null)).isEqualTo(3000);
   }
 
   @Test
-  void testToInitializeBufferEnabled() throws Exception {
+  void getBufferEnabled() {
     assertThat(FACTORY.getBufferEnabled(null)).isFalse();
   }
 
   @Test
-  void testToInitializeBufferDir() {
+  void getBuffer() {
     Buffer expect = new DiskBuffer(new File("./buffer"), 100);
     assertThat(FACTORY.getBuffer(null)).isEqualToComparingFieldByField(expect);
   }
 
   @Test
-  void testToInitializeBufferSize() throws Exception {
-    Method method = DefaultSentryClientFactory.class.getDeclaredMethod("getBufferSize", Dsn.class);
-    method.setAccessible(true);
-    int bufferSize = (int) method.invoke(FACTORY, (Dsn) null);
-    assertThat(bufferSize).isEqualTo(100);
+  void getBufferSize() {
+    assertThat(FACTORY.getBufferSize(null)).isEqualTo(100);
   }
 
   @Test
-  void testToInitializeBufferFlushTime() throws Exception {
-    Method method =
-        DefaultSentryClientFactory.class.getDeclaredMethod("getBufferFlushtime", Dsn.class);
-    method.setAccessible(true);
-    long bufferFlushTime = (long) method.invoke(FACTORY, (Dsn) null);
-    assertThat(bufferFlushTime).isEqualTo(200000);
+  void getUncaughtHandlerEnabled() {
+    assertThat(FACTORY.getUncaughtHandlerEnabled(null)).isFalse();
   }
 
   @Test
-  void testToInitializeBufferShutdownTimeout() throws Exception {
-    Method method =
-        DefaultSentryClientFactory.class.getDeclaredMethod(
-            "getBufferedConnectionShutdownTimeout", Dsn.class);
-    method.setAccessible(true);
-    long bufferFlushTime = (long) method.invoke(FACTORY, (Dsn) null);
-    assertThat(bufferFlushTime).isEqualTo(300000);
-  }
-
-  @Test
-  void testToInitializeGracefulShutdownEnabled() {
-    assertThat(FACTORY.getBufferedConnectionGracefulShutdownEnabled(null)).isFalse();
-  }
-
-  @Test
-  void testToInitializeAsyncEnabled() {
-    assertThat(FACTORY.getAsyncEnabled(null)).isFalse();
-  }
-
-  @Test
-  void testToIntializeAsyncShutdownTimeout() throws Exception {
-    Method method =
-        DefaultSentryClientFactory.class.getDeclaredMethod("getAsyncShutdownTimeout", Dsn.class);
-    method.setAccessible(true);
-    long isEnabled = (long) method.invoke(FACTORY, (Dsn) null);
-    assertThat(isEnabled).isEqualTo(500L);
-  }
-
-  @Test
-  void testConfigToMapThrowAnErrorIfSetInvalidConfigurationType() {
+  void configToMap() {
     Config config = ConfigFactory.parseString("hoge = { invalid = 1234.56 }");
     assertThatExceptionOfType(IllegalArgumentException.class)
         .isThrownBy(() -> SentryClientFactory.configToMap(config))
